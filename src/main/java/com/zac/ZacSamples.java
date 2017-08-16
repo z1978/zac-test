f86429767282d60429fd8250e5cba3ea8b5c0669
z1978 <zgithub@hotmail.com>
2017-08-16 09:46:32 +0900

    Update ZacSamples.java
    add while

--- src/main/java/com/zac/ZacSamples.java
+++ src/main/java/com/zac/ZacSamples.java
@@ -5,6 +5,7 @@ import java.io.*;
 class ZacSamples { 
     public static void main(String[] args) throws java.lang.Exception {
         // breakを使って強制的に終了させる方法
+        System.out.println("=== break ===");
         for (int i = 1; i <= 10; i++) {
             if (i > 5) {
                 break;
@@ -13,11 +14,25 @@ class ZacSamples { 
         }
         
         // continueを使ってその周の処理だけをスキップして、次の周を実行する
+        System.out.println("=== continue ===");
         for (int i = 1; i <= 10; i++) {
             if (i % 3 == 0) {
                 continue;
             }
             System.out.println(i);
         }
+        
+        // while
+        System.out.println("=== while文 ===");
+        int i = 1;
+        while (i < 10) {
+            // iが5の倍数のとき、繰り返し処理を終了してください
+            if (i % 5 == 0) {
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+        
     }
 }
